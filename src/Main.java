66a26abc2c80569a314917e3b1219ce3772bb98e
chebelok <lotokhinivan@gmail.com>
2023-03-07 05:05:05 +0200

    hello world commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) {
         if (args.length == 0) Equation.iMode(); else Equation.fMode(args);
+        System.out.println("Hello world");
 
     }
 }
