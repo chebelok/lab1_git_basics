63b4e40359c7117d2e4dd4aa441e3258c905640b
chebelok <lotokhinivan@gmail.com>
2023-03-07 04:34:36 +0200

    added if statement for choosing mode

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-
+        if (args.length == 0) Equation.iMode(); else Equation.fMode(args);
 
     }
 }
