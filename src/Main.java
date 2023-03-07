fa87631b045c8602a5aac58c750985ff8824cfaa
chebelok <lotokhinivan@gmail.com>
2023-03-07 05:27:21 +0200

    Revert "hello world commit"
    This reverts commit 66a26abc

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) {
         if (args.length == 0) Equation.iMode(); else Equation.fMode(args);
-        System.out.println("Hello world");
 
     }
 }
